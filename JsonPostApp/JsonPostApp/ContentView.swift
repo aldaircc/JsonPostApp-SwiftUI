1b535a57fcccf65ea4721e3c8e980105b67a409b
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-03-14 22:01:46 -0500

    Initial commit

---
+++ JsonPostApp/JsonPostApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JsonPostApp
+//
+//  Created by Aldair Cosetito Coral on 14/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JsonPostApp/JsonPostApp/JsonPostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonPostAppApp.swift
+//  JsonPostApp
+//
+//  Created by Aldair Cosetito Coral on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct JsonPostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

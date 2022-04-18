2e980979668e8a0ba267d870a832c2d196057293
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-04-17 22:50:04 -0500

    Component segmented view
    In progress to do it dynamically

--- JsonPostApp/JsonPostApp/View/Components/SegmentedControlView.swift
+++ JsonPostApp/JsonPostApp/View/Components/SegmentedControlView.swift
@@ -7,12 +7,25 @@
 
 import SwiftUI
 
+struct SegmentedItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let index: Int
+}
+
 struct SegmentedControlView: View {
     // MARK: - Variables
     @State private var tabSelected:Int = 1
     @State private var currentPosition:CGFloat = 0
+    var options: [String]
+    var items: [SegmentedItem] = []
     
-    init() {
+    init(options: [String]) {
+        self.options = options
+        
+        for option in options {
+            self.items.append(SegmentedItem.init(name: option, index: self.items.count + 1))
+        }
     }
     
     var body: some View {
@@ -28,47 +41,82 @@ struct SegmentedControlView: View {
             .padding()
             
             GeometryReader { proxy in
-                ZStack(alignment: .center) {
+                VStack {
                     
-                    RoundedRectangle(cornerRadius: 5)
-                        .size(width: proxy.size.width, height: 30)
-                        .foregroundColor(Color.gray)
-                    
-                    RoundedRectangle(cornerRadius: 5)
-                        .size(width: proxy.size.width/4, height: 30)
-                        .offset(x: currentPosition,
-                                y: 0)
-                        .animation(.easeOut, value: currentPosition)
-                    
-                    HStack(alignment: .center, spacing: proxy.size.width/4 - 60) {
+                    ZStack(alignment: .center) {
+                        RoundedRectangle(cornerRadius: 5)
+                            .size(width: proxy.size.width, height: 30)
+                            .foregroundColor(Color.gray)
                         
-                        Button(action: {
-                            self.currentPosition =  (proxy.size.width/4) * 0
-                        }) {
-                            Text("Option 1")
-                        }
+                        RoundedRectangle(cornerRadius: 5)
+                            .size(width: proxy.size.width/CGFloat(items.count), height: 30)
+                            .offset(x: currentPosition,
+                                    y: 0)
+                            .animation(.easeOut, value: currentPosition)
                         
-                        Button(action: {
-                            self.currentPosition = (proxy.size.width/4) * 1
-                        }) {
-                            Text("Option 2")
+                        HStack(alignment: .center, spacing: proxy.size.width/CGFloat(items.count) - 60) {
+                            
+                            ForEach(items, id: \.id) { option in
+                                
+                                Button(action: {
+//                                    self.currentPosition =  (proxy.size.width/CGFloat(option.index)) * 0
+                                    
+                                    self.currentPosition =  (proxy.size.width/CGFloat(items.count)) * CGFloat(option.index - 1)
+                                }) {
+                                    Text(option.name)
+                                }
+                            }
+                            
                         }
+                        .padding([.top], 4)
                         
-                        Button(action: {
-                            self.currentPosition = (proxy.size.width/4) * 2
-                        }) {
-                            Text("Option 3")
-                        }
-                        
-                        Button(action: {
-                            self.currentPosition = (proxy.size.width/4) * 3
-                        }) {
-                            Text("Option 4")
-                        }
                     }
-                    .padding([.top], 4)
+                    .frame(width: .infinity, height: 20)
+                    .padding(.bottom)
+                    
+                    
+                    ZStack(alignment: .center) {
+                        
+                        RoundedRectangle(cornerRadius: 5)
+                            .size(width: proxy.size.width, height: 30)
+                            .foregroundColor(Color.gray)
+                        
+                        RoundedRectangle(cornerRadius: 5)
+                            .size(width: proxy.size.width/4, height: 30)
+                            .offset(x: currentPosition,
+                                    y: 0)
+                            .animation(.easeOut, value: currentPosition)
+                        
+                        HStack(alignment: .center, spacing: proxy.size.width/4 - 60) {
+                            
+                            Button(action: {
+                                self.currentPosition =  (proxy.size.width/4) * 0
+                            }) {
+                                Text("Option 1")
+                            }
+                            
+                            Button(action: {
+                                self.currentPosition = (proxy.size.width/4) * 1
+                            }) {
+                                Text("Option 2")
+                            }
+                            
+                            Button(action: {
+                                self.currentPosition = (proxy.size.width/4) * 2
+                            }) {
+                                Text("Option 3")
+                            }
+                            
+                            Button(action: {
+                                self.currentPosition = (proxy.size.width/4) * 3
+                            }) {
+                                Text("Option 4")
+                            }
+                        }
+                        .padding([.top], 4)
+                    }
+                    .frame(width: .infinity, height: 20)
                 }
-                .frame(width: .infinity, height: 20)
             }
             
         }
@@ -77,6 +125,6 @@ struct SegmentedControlView: View {
 
 struct SegmentedControlView_Previews: PreviewProvider {
     static var previews: some View {
-        SegmentedControlView()
+        SegmentedControlView(options: ["All", "Favorites"])
     }
 }
